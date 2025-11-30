747e416664d83d65a5fb346ee2277fd55b812206
Hector R. <github.hector1@gmail.com>
2025-11-29 20:12:41 -0600

    Added GlobalConfig.cs to enable easily initiative the .net framework 4.0 default encoding.

---
+++ GlobalConfig.cs
@@ -0,0 +1,6 @@
+namespace WhiteBinTools.WhiteBinTools;
+
+public static class GlobalConfig
+{
+    public static readonly System.Text.Encoding DefaultEncoding = System.Text.Encoding.GetEncoding(1252);
+}
